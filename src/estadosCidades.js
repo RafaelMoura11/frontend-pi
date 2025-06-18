b2ea830361c0f2f6e41589944935d7b1322f3885
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2025-06-17 21:48:43 -0300

    Add estadosCidades data structure for state and city mapping

---
+++ src/estadosCidades.js
@@ -0,0 +1,29 @@
+const estadosCidades = {
+  "Alabama": [
+    "Abbeville",
+    "Andalusia",
+    "Anderson"
+  ],
+  "Alaska": [
+    "Anchorage",
+    "North Pole",
+    "Seward"
+  ],
+  "Arizona": [
+    "Buckeye",
+    "Bullhead City",
+    "Carefree"
+  ],
+  "California": [
+    "Los Angeles",
+    "San Diego",
+    "San Francisco"
+  ],
+  "Texas": [
+    "Austin",
+    "Dallas",
+    "Houston"
+  ]
+};
+
+export default estadosCidades;
